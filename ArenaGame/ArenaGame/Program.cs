34afca13b34f49be8fa5fc4d48c0969323591905
Melendez, Cory <cory.melendez@outlook.com>
2019-04-26 13:48:27 -0500

    Solution Setup

---
+++ ArenaGame/ArenaGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArenaGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Welcome to Arena!!!----------");
+            Console.WriteLine("Theres nothing to do...yet");
+            Console.WriteLine("Press any button to exit...");
+
+            var input = Console.ReadLine();
+        }
+    }
+}
